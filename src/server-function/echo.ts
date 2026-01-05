5dfa4198c5445c19c40c07bf6523899406eb657c
Shellishack <40737228+Shellishack@users.noreply.github.com>
2026-01-06 01:05:47 +0800

    update packages

--- src/server-function/echo.ts
+++ src/server-function/echo.ts
@@ -12,3 +12,11 @@ export default async function echo(req: Request) {
   // Process the data and return a response
   return new Response(JSON.stringify(data), { status: 200 });
 }
+
+/* 
+  If you want to charge credits for using this server function,
+  uncomment the line below and set the desired credit cost.
+  Each unique API endpoint's cost cannot be changed after deployment.
+  To update costs, you must deploy a new endpoint. 
+*/
+// export const _CREDIT_PER_CALL = 1;
